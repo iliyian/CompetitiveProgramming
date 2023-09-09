317c626f822ae08b193f69a3c499e895f308af1e
iliyian <sqzrmhj@gmail.com>
2023-09-09 09:59:40 +0800

    Day 62

--- Problems/P3387/main.cpp
+++ Problems/P3387/main.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> g;
+vector<vector<int>> g, scg;
 
-array<int, 10001> a, dfn, low, s, in, pa, aa;
-bitset<10001> ins;
-int cnt = 0, tp = 0;
+array<int, 10001> a, dfn, low, s, in, dist, scc, sca, f;
+bitset<10001> ins, vis;
+int cnt = 0, tp = 0, sc = 0;
 
 void tarjan(int u) {
   dfn[u] = low[u] = ++cnt;
@@ -20,12 +20,23 @@ void tarjan(int u) {
     }
   }
   if (dfn[u] == low[u]) {
+    sc++;
     do {
-      pa[s[tp]] = u;
-      aa[u] += a[s[tp]];
       ins[s[tp]] = false;
+      scc[s[tp]] = sc;
     } while (s[tp--] != u);
   }
+}
+
+void dfs(int u) {
+  if (f[u]) return;
+  f[u] = sca[u];
+  int sum = 0;
+  for (int v : scg[u]) {
+    dfs(v);
+    sum = max(sum, f[v]);
+  }
+  f[u] += sum;
 }
 
 int main() {
@@ -34,33 +45,32 @@ int main() {
   int n, m;
   cin >> n >> m;
   for (int i = 1; i <= n; i++)
-    cin >> a[i], pa[i] = i;
+    cin >> a[i];
   g.resize(n + 1);
+  scg.resize(n + 1);
   for (int i = 0; i < m; i++) {
     int u, v;
     cin >> u >> v;
     g[u].push_back(v);
-    in[v]++;
   }
 
   for (int i = 1; i <= n; i++)
     if (!dfn[i])
       tarjan(i);
 
-  queue<int> q;
-  vector<int> L;
-  for (int i = 1; i <= n; i++) {
-    if (pa[i] == i)
-      q.push(i);
-  }
-  
-  while (!q.empty()) {
-    int u = q.front(); q.pop();
-    L.push_back(u);
-    for (int v : g[u])
-      if (!--in[v])
-        q.push(v);
-  }
+  for (int i = 1; i <= n; i++)
+    sca[scc[i]] += a[i];
 
+  queue<int> q;
+
+  for (int i = 1; i <= n; i++)
+    for (int v : g[i])
+      if (scc[i] != scc[v])
+        scg[scc[i]].push_back(scc[v]);
+  
+  int ans = -1;
+  for (int i = 1; i <= sc; i++)
+    dfs(i), ans = max(ans, f[i]);
+  cout << ans;
   return 0;
 }
