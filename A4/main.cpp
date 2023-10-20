6959e0294037bbd573bf96129afd9e9032a43fc6
iliyian <sqzrmhj@gmail.com>
2023-10-20 16:34:17 +0800

    First commit

---
+++ A4/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  if (n < 4) cout << "NO";
+  else if (n & 1) cout << "NO";
+  else cout << "YES";
+  return 0;
+}
