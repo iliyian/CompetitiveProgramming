501580312edf2ad78efa4e265d0a2f8f1d9018df
iliyian <sqzrmhj@gmail.com>
2023-11-24 15:35:05 +0800

    Day 128

--- Problems/P1197_wip/main.cpp
+++ Problems/P1197_wip/main.cpp
@@ -2,26 +2,39 @@
 #define N 200005
 using namespace std;
 
-int pa[N];
+int pa[N], k[N];
+bitset<N> destroyed;
 
 int find(int x) {return x == pa[x] ? x : pa[x] = find(pa[x]);}
 void unite(int x, int y) {pa[find(x)] = find(y);}
 
-vector<vector<int>> g;
+struct Edge {
+  int u, v;
+};
+vector<Edge> g;
 
 int main() {
   ios::sync_with_stdio(false); cin.tie(0);
   freopen("data.in", "r", stdin);
-  int n, m, k;
+  int n, m, K;
   cin >> n >> m;
   iota(pa, pa + n, 0);
   for (int i = 0; i < m; i++) {
     int x, y;
     cin >> x >> y;
-    g[x].push_back(y);
-    g[y].push_back(x);
-    
+    g.push_back({x, y});
   }
-  cin >> k;
+  cin >> K;
+  for (int i = 0; i < K; i++) {
+    cin >> k[i];
+    destroyed[k[i]] = true;
+  }
+  int cnt = n - K;
+  for (auto &[u, v] : g) {
+    if (!destroyed[u] && !destroyed[v] && find(u) != find(v)) {
+      pa[find(u)] = find(v);
+      cnt--;
+    }
+  }
   return 0;
 }
