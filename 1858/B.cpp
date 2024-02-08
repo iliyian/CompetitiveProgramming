ec9fc4e985e535818b3198cb6911cfc41a307153
iliyian <sqzrmhj@gmail.com>
2024-02-08 16:32:19 +0800

    add 1858B-C, 1859A-C, 1927F

--- 1858/B.cpp
+++ 1858/B.cpp
@@ -1,21 +1,33 @@
+// date: 2024/02/08 12:18:44
+// tag: 简单模拟，不能想复杂了
+
 #include <bits/stdc++.h>
 using namespace std;
 
 void solve() {
   int n, m, d;
   cin >> n >> m >> d;
-  vector<int> a(n + 1);
-  int ans1 = 0;
+  vector<int> a(m + 2), s(m + 2), t(m + 1);
   bool f = false;
+  a[0] = 1, a[m + 1] = n + 1; // 开区间
   for (int i = 1; i <= m; i++) {
     cin >> a[i];
-    if ((a[i] - 1) % d) {
-      ans1++;
-      f = true;
-    }
   }
-  ans1 += (n - 1) / d - f;
-  cout << ans1 << '\n';
+  int sum = 0;
+  for (int i = 1; i <= m + 1; i++) {
+    s[i] = ceil(double(a[i] - a[i - 1]) / d);
+    sum += s[i];
+  }
+  int mi = 0x3f3f3f3f;
+  for (int i = 1; i <= m; i++) {
+    t[i] = sum - s[i] - s[i + 1] + ceil(double(a[i + 1] - a[i - 1]) / d);
+    mi = min(mi, t[i]);
+  }
+  int ans = 0;
+  for (int i = 1; i <= m; i++) {
+    ans += mi == t[i];
+  }
+  cout << mi << ' ' << ans << '\n';
 }
 
 int main() {
---
+++ 1927/F.cpp
@@ -0,0 +1,76 @@
+// date: 2024/02/08 12:56:26
+// tag: lambda函数也可以递归
+
+#include <bits/stdc++.h>
+#define N 100005
+using namespace std;
+
+struct Edge {
+  int u, v, w;
+};
+
+struct Ed {
+  int v, w;
+};
+
+int pa[N];
+int find(int x) {return x == pa[x] ? x : pa[x] = find(pa[x]);}
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  vector<Edge> vec;
+  vector<vector<int>> g;
+  g.resize(n + 1);
+  for (int i = 1; i <= m; i++) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    vec.push_back({u, v, w});
+  }
+  sort(vec.begin(), vec.end(), [&](const Edge &x, const Edge &y) {
+    return x.w > y.w;
+  });
+  int U = -1, V = -1, mi = 0x3f3f3f3f;
+  iota(pa + 1, pa + 1 + n, 1);
+  
+  for (auto [u, v, w] : vec) {
+    int pu = find(u), pv = find(v);
+    if (pu != pv) {
+      g[u].push_back(v);
+      g[v].push_back(u);
+      pa[pu] = pv;
+    } else {
+      mi = w;
+      U = u, V = v;
+    }
+  }
+  vector<int> ans;
+  auto dfs = [&](auto self, int u, int p) {
+    if (u == V) {
+      ans.push_back(u);
+      return true;
+    }
+    for (int v : g[u]) {
+      if (v == p) continue;
+      if (self(self, v, u)) {
+        ans.push_back(u);
+        return true;
+      }
+    }
+    return false;
+  };
+  dfs(dfs, U, -1);
+  cout << mi << ' ' << ans.size() << '\n';
+  for (auto i : ans) {
+    cout << i << ' ';
+  }
+  cout << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false); cin.tie(0);
+  freopen("F.in", "r", stdin);
+  freopen("F.out", "w", stdout);
+  int _; cin >> _;
+  while (_--) solve();
+}
