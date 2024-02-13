c777d25e438c2937e964e02ea1a82d158f222108
iliyian <sqzrmhj@gmail.com>
2024-02-13 14:42:18 +0800

    add two

--- 1928/C.cpp
+++ 1928/C.cpp
@@ -1,29 +1,43 @@
+// date: 2024/02/12 22:42:17
+// tag: wa#01: 没有考虑到因子是(k-1)而不是k，需要特殊处理
+// (n+x)/2=(k-1)t+k -> (n+x)/2-1=(k-1)(t-1) x<=k+1
+// (n-x)/2=(k-1)t x<=k
+
 #include <bits/stdc++.h>
 using namespace std;
 
 void solve() {
   int n, x;
   cin >> n >> x;
-  int t = (n - x);
-  if (t % 2) {
+  if ((n + x) % 2 != 0) {
     cout << 0 << '\n';
     return;
   }
-  t /= 2;
-  int ans = 0;
-  for (int i = 2; i * i <= t; i++) {
-    if (t % i == 0) {
-      t /= i;
-      ans++;
+  int d = (n - x) / 2, ans = 0;
+  for (int k = 1; k * k <= d; k++) {
+    if (d % k != 0) {
+      continue;
+    }
+    ans += (x < k + 1);
+    // 另外半边
+    if (k * k != d) {
+      ans += (x < d / k + 1);
+    }
+  }
+  int e = (n + x) / 2 - 1;
+  if (x > 1) {
+    for (int k = 1; k * k <= e; k++) {
+      if (e % k != 0) {
+        continue;
+      }
+      ans += (x <= k + 1);
+      if (k * k != e) {
+        ans += (x <= e / k + 1);
+      }
     }
   }
   cout << ans << '\n';
 }
-// 7 -> 5
-// 8 -> 4
-// 1 2 3 4 5 6 5 4 3 2
-// 
-
 
 int main() {
   ios::sync_with_stdio(false); cin.tie(0);
