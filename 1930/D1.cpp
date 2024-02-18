f77c140ddbe273924f9cabe0143498ffe0416b20
iliyian <sqzrmhj@gmail.com>
2024-02-18 19:10:15 +0800

    1930 failure

---
+++ 1930/D1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  string str;
+  cin >> str;
+  int ans = 0;
+  for (int l = 1; l <= n; l++) {
+    
+  }
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  freopen("D1.in", "r", stdin);
+  freopen("D1.out", "w", stdout);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
