6080a48d20b98753643687bb9dc0fcfc51fce0fe
iliyian <sqzrmhj@gmail.com>
2024-02-28 17:56:43 +0800

    add final exam

---
+++ exam/1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  cout << "\" \\寒假快乐!/ \"";
+}
+
+int main() {
+  ios::sync_with_stdio(false); cin.tie(0);
+  // freopen("1.in", "r", stdin);
+  // freopen("1.out", "w", stdout);
+  // int _; cin >> _;
+  int _ = 1;
+  while (_--) solve();
+}
