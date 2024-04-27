f9337a9ee6222f1dabd59b177e012024fbdb3553
iliyian <i@iliyian.com>
2024-04-27 15:34:51 +0800

    rename sakir -> saikr

---
+++ saikr.com/1456/D.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  freopen("D.in", "r", stdin);
+  freopen("D.out", "w", stdout);
+
+  int a, b, mod = 1000;
+  cin >> a >> b;
+  int ans = 1;
+  while (b) {
+    if (b & 1) ans = ans * a % mod;
+    a = a * a % mod;
+    b >>= 1;
+  }
+  cout << setw(3) << setfill('0') << ans << '\n';
+
+  return 0;
+}
